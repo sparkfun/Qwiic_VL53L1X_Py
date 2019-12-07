5484b6e9b5f856aacdd9566a66223c6eac2ab718
santaimpersonator <36016723+santaimpersonator@users.noreply.github.com>
2019-12-06 20:13:02 -0700

    Update Examples
    Add licensing

--- examples/Example1_ReadDistance.py
+++ examples/Example1_ReadDistance.py
@@ -1,17 +1,57 @@
+#-----------------------------------------------------------------------
+# VL53L1X - Example 1
+#-----------------------------------------------------------------------
+#
+# Ported by  SparkFun Electronics, October 2019
+# Author: Nathan Seidle
+# Ported: Wes Furuya
+# SparkFun Electronics
+# 
+# License: This code is public domain but you buy me a beer if you use
+# this and we meet someday (Beerware license).
+#
+# Compatibility: https://www.sparkfun.com/products/14722
+# 
+# Do you like this library? Help support SparkFun. Buy a board!
+# For more information on VL53L1x (ToF), check out the product page
+# linked above.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http:www.gnu.org/licenses/>.
+#
+#=======================================================================
+# Copyright (c) 2019 SparkFun Electronics
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#=======================================================================
+
 """
-Reading distance from the laser based VL53L1X
-By: Nathan Seidle
-Ported: Wes Furuya
-SparkFun Electronics
-Date: October 31, 2019
-License: This code is public domain but you buy me a beer if you use this and we meet someday (Beerware license).
+	Reading distance from the laser based VL53L1X
 
-SparkFun labored with love to create this code. Feel like supporting open source hardware?
-Buy a board from SparkFun! https://www.sparkfun.com/products/14667
-
-This example prints the distance to an object.
-
-Are you getting weird readings? Be sure the vacuum tape has been removed from the sensor.
+	This example prints the distance to an object. If you are getting weird
+	readings, be sure the vacuum tape has been removed from the sensor.
 """
 
 import qwiic
