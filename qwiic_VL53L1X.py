c410445a0de2c5bff1941805f44872be15297bce
santaimpersonator <36016723+santaimpersonator@users.noreply.github.com>
2020-01-15 15:11:04 -0700

    add ROI Center
    Add programmable center for ROI per ST user manual

--- qwiic_VL53L1X.py
+++ qwiic_VL53L1X.py
@@ -1195,16 +1195,39 @@ class QwiicVL53L1X(object):
 		return high
 
 
-	def SetROI(self, X, Y):
+	def SetROI(self, X, Y, OpticalCenter = 199):
 		"""
-		This function programs the ROI (Region of Interest)
-		The ROI position is centered, only the ROI size can be reprogrammed.
-		The smallest acceptable ROI size = 4
+		This function programs the ROI (Region of Interest). The height and width of the ROI (X, Y) are set in SPADs; the smallest acceptable ROI size = 4 (2 x 2?). The optical center is set based on table below.
+		To set the center, use the pad that is to the right and above (i.e. upper right of) the exact center of the region you'd like to measure as your optical center.
+
+		Table of Optical Centers:
+		
+			128,136,144,152,160,168,176,184,  192,200,208,216,224,232,240,248
+			129,137,145,153,161,169,177,185,  193,201,209,217,225,233,241,249
+			130,138,146,154,162,170,178,186,  194,202,210,218,226,234,242,250
+			131,139,147,155,163,171,179,187,  195,203,211,219,227,235,243,251
+			132,140,148,156,164,172,180,188,  196,204,212,220,228,236,244,252
+			133,141,149,157,165,173,181,189,  197,205,213,221,229,237,245,253
+			134,142,150,158,166,174,182,190,  198,206,214,222,230,238,246,254
+			135,143,151,159,167,175,183,191,  199,207,215,223,231,239,247,255
+			
+			127,119,111,103, 95, 87, 79, 71,  63, 55, 47, 39, 31, 23, 15, 7
+			126,118,110,102, 94, 86, 78, 70,  62, 54, 46, 38, 30, 22, 14, 6
+			125,117,109,101, 93, 85, 77, 69,  61, 53, 45, 37, 29, 21, 13, 5
+			124,116,108,100, 92, 84, 76, 68,  60, 52, 44, 36, 28, 20, 12, 4
+			123,115,107, 99, 91, 83, 75, 67,  59, 51, 43, 35, 27, 19, 11, 3
+			122,114,106, 98, 90, 82, 74, 66,  58, 50, 42, 34, 26, 18, 10, 2
+			121,113,105, 97, 89, 81, 73, 65,  57, 49, 41, 33, 25, 17,  9, 1
+			120,112,104, 96, 88, 80, 72, 64,  56, 48, 40, 32, 24, 16,  8, 0 Pin 1
+			(Each SPAD has a number which is not obvious.)
+
 		:param X:	ROI Width
 		:param Y:	ROI Height
+		:param OpticalCenter: The pad that is to the upper right of the exact center of the ROI (see table above).
+			Default = 199
 		"""
 		self.status = 0
-		OpticalCenter =self.__i2cRead(self.address, VL53L1_ROI_CONFIG__MODE_ROI_CENTRE_SPAD, 1)
+		# OpticalCenter =self.__i2cRead(self.address, VL53L1_ROI_CONFIG__MODE_ROI_CENTRE_SPAD, 1)
 		if (X > 16):
 			X = 16
 		if (Y > 16):
